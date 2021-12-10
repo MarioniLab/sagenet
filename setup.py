a061f854d353299dff0d981db4d68d251d56ee7f
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2021-12-10 14:53:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.0+cu113.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.0+cu113.html
+pip install torch-geometric
+
 from pathlib import Path
 
 from setuptools import setup, find_packages
