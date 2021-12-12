7eed7399538c7ea56896d17ffdafa389b6a1d3d1
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2021-12-12 10:17:04 +0100

    Update __init__.py

--- sagenet/__init__.py
+++ sagenet/__init__.py
@@ -1,5 +1,5 @@
 __author__ = __maintainer__ = "Elyas Heidari"
-__email__ = ['eheidari@student.ethz.ch']
+__email__ = 'eheidari@student.ethz.ch'
 __version__ = "0.1.0"
 
 from . import classifier, model, sage, utils, datasets
