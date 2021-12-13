417bd443daaf770f52bf320c96f2cc8a9b998576
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2021-12-13 17:55:45 +0100

    Update _datasets.py

--- sagenet/datasets/_datasets.py
+++ sagenet/datasets/_datasets.py
@@ -5,7 +5,7 @@ _MGA = AMetadata(
     name="MGA",
     doc_header="",
     # shape=(270876, 43),
-    url="https://figshare.com/ndownloader/files/31766651",
+    url="https://figshare.com/ndownloader/files/31767704",
 )
 
 _seqFISH1 = AMetadata(
