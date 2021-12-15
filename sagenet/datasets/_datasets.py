921aaf9dd89a9f5a724d9b9bcd9f6bfdffcd22a2
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2021-12-15 09:30:14 +0100

    Update _datasets.py

--- sagenet/datasets/_datasets.py
+++ sagenet/datasets/_datasets.py
@@ -1,21 +1,21 @@
 from copy import copy
 from squidpy.datasets._utils import AMetadata
 
-_MGA = AMetadata(
+_MGA_scRNAseq = AMetadata(
     name="MGA",
     doc_header="",
     # shape=(270876, 43),
     url="https://figshare.com/ndownloader/files/31767704",
 )
 
-_seqFISH1 = AMetadata(
+_MGA_seqFISH1 = AMetadata(
     name="seqFISH1",
     doc_header="",
     # shape=(270876, 43),
     url="https://figshare.com/ndownloader/files/31716029",
 )
 
-_seqFISH2 = AMetadata(
+_MGA_seqFISH2 = AMetadata(
     name="seqFISH2",
     doc_header="",
     # shape=(270876, 43),
@@ -23,12 +23,28 @@ _seqFISH2 = AMetadata(
 )
 
 
-_seqFISH3 = AMetadata(
+_MGA_seqFISH3 = AMetadata(
     name="seqFISH3",
     doc_header="",
     # shape=(270876, 43),
     url="https://figshare.com/ndownloader/files/31716089",
 )
+
+_DHH_visium = AMetadata(
+    name="dhh_visium",
+    doc_header="https://figshare.com/ndownloader/files/31796207",
+    # shape=(270876, 43),
+    url="https://figshare.com/ndownloader/files/31716089",
+)
+
+_DHH_scRNAseq = AMetadata(
+    name="scRNAseq2",
+    doc_header="https://figshare.com/ndownloader/files/31796219",
+    # shape=(270876, 43),
+    url="https://figshare.com/ndownloader/files/31716089",
+)
+
+
 
 
 for name, var in copy(locals()).items():
@@ -37,8 +53,11 @@ for name, var in copy(locals()).items():
 
 
 __all__ = [  # noqa: F822
-    "MGA",
-    "seqFISH1",
-    "seqFISH2",
-    "seqFISH3",
+    "MGA_scRNAseq",
+    "MGA_seqFISH1",
+    "MGA_seqFISH2",
+    "MGA_seqFISH3",
+    'DHH_visium',
+    'DHH_scRNAseq'
+    
 ]
