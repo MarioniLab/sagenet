e23946a1a240836b8b35e09c365b8f0e15ef2f26
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2022-03-18 11:14:13 +0100

    Update sage.py

--- sagenet/sage.py
+++ sagenet/sage.py
@@ -91,8 +91,8 @@ class sage():
 
             clf.fit(data_loader, epochs = epochs, test_dataloader=None,verbose=verbose)
             imp = clf.interpret(data_loader, n_features=adata.shape[1], n_classes=(np.max(adata.obs[comm].values.astype('long'))+1))
-            idx = (-abs(imp)).argsort(axis=0) 
-            imp = np.min(idx, axis=1)
+#             idx = (-abs(imp)).argsort(axis=0) 
+            imp = np.sum(idx, axis=0)
             ents += imp
             self.models['_'.join([tag, comm])] = clf.net
             self.adjs['_'.join([tag, comm])] = adata.varm['adj'].toarray()
