154bc3ce839c4d91035b3dabd8ec746b3de9bcff
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2022-03-18 09:36:55 +0100

    Update sage.py

--- sagenet/sage.py
+++ sagenet/sage.py
@@ -62,7 +62,7 @@ class sage():
             Also adds a new key `{tag}_entropy` to `.var` from `adata` which contains  the entropy values as the importance score corresponding to each gene.
         """    
         ind = np.where(np.sum(adata.varm['adj'], axis=1))[0]
-        ents = np.zeros(ind.shape[0])
+        ents = np.zeros(adata.var.shape[0])
         self.num_refs += 1
 
         if tag is None:
@@ -91,7 +91,7 @@ class sage():
 
             clf.fit(data_loader, epochs = epochs, test_dataloader=None,verbose=verbose)
             imp = clf.interpret(data_loader, n_features=adata.shape[1], n_classes=(np.max(adata.obs[comm].values.astype('long'))+1))
-            idx = (-abs(imp)[ind,:]).argsort(axis=0) 
+            idx = (-abs(imp)).argsort(axis=0) 
             imp = np.min(idx, axis=1)
             ents  += imp
             self.models['_'.join([tag, comm])] = clf.net
