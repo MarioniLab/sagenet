50661e8eeef2e2fbd0cb5969569a11e370e5e180
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2022-03-18 07:58:39 +0100

    edited markers to ranked based

--- sagenet/sage.py
+++ sagenet/sage.py
@@ -92,11 +92,11 @@ class sage():
             clf.fit(data_loader, epochs = epochs, test_dataloader=None,verbose=verbose)
             imp = clf.interpret(data_loader, n_features=adata.shape[1], n_classes=(np.max(adata.obs[comm].values.astype('long'))+1))
             idx = (-abs(imp)[ind,:]).argsort(axis=0) 
-            imp = np.mean(idx, axis=1)
+            imp = np.min(idx, axis=1)
             ents  += imp
             self.models['_'.join([tag, comm])] = clf.net
             self.adjs['_'.join([tag, comm])] = adata.varm['adj'].toarray()
-        save_adata(adata, attr='var', key='_'.join([tag, 'entropy']), data=ents)
+        save_adata(adata, attr='var', key='_'.join([tag, 'importance']), data=ents)
 #         return ents
 
 
