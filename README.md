385062e9346217d885df7b6cfd192859fb88d36e
Elihei2 <eheidari@student.ethz.ch>
2024-01-10 14:02:26 +0100

    new utils for direct mapping, updated glasso

---
+++ README.md
@@ -0,0 +1,11 @@
+
+SageNet package:
+    [ ] Code
+        [ ] Refactor and update
+            [ ] PyG data and classifiers
+        [ ] New Function
+            [ ] distance function
+            [ ] new benchmark metrics
+        [ ] Refactor
+
+    [ ] Documentation
--- sagenet/utils.py
+++ sagenet/utils.py
@@ -2,17 +2,20 @@ import os
 import csv
 import numpy as np
 from sklearn.metrics import *
-from sklearn.covariance import GraphicalLassoCV, graphical_lasso, LedoitWolf
-from sklearn.preprocessing import StandardScaler
+from gglasso.helper.data_generation import generate_precision_matrix, group_power_network, sample_covariance_matrix
+from gglasso.problem import glasso_problem
+from gglasso.helper.basic_linalg import adjacency_matrix
 import torch
 import torch_geometric.data as geo_dt 
 from sklearn.utils.extmath import fast_logdet
 import numpy as np
 from scipy import sparse
+import warnings
+from torch.nn import Softmax
 
 
 
-def glasso(adata, alphas=5, n_jobs=None, mode='cd'):
+def glasso(adata, lambda_low=-10, lambda_high=-1, mode='cd'):
     """
         Recustructs the gene-gene interaction network based on gene expressions in `.X` using a guassian graphical model estimated by `glasso`. 
 
@@ -35,13 +38,19 @@ def glasso(adata, alphas=5, n_jobs=None, mode='cd'):
         Sparse inverse covariance estimation with the graphical lasso. 
         Biostatistics, 9(3), 432-441.
     """
+    N = adata.shape[1]
     scaler = StandardScaler()
     data = scaler.fit_transform(adata.X)
-    cov = GraphicalLassoCV(alphas=alphas, n_jobs=n_jobs).fit(data)
-    precision_matrix = cov.get_precision()
-    adjacency_matrix = precision_matrix.astype(bool).astype(int)
-    adjacency_matrix[np.diag_indices_from(adjacency_matrix)] = 0
-    save_adata(adata, attr='varm', key='adj', data=sparse.csr_matrix(adjacency_matrix))
+    S    = empirical_covariance(data)
+    P    = glasso_problem(S, N, latent = False, do_scaling = True)
+    # lambda1_range = np.logspace(-0.1, -1, 10)
+    lambda1_range = np.logspace(-10, -1,10)
+    modelselect_params = {'lambda1_range': lambda1_range}
+    P.model_selection(modelselect_params = modelselect_params, method = 'eBIC', gamma = 0.1, tol=1e-7)
+    sol = P.solution.precision_
+    P.solution.calc_adjacency(t = 1e-4)
+    save_adata(adata, attr='varm', key='adj', data=sparse.csr_matrix(P.solution.precision_))
+
 
 
 def compute_metrics(y_true, y_pred):
@@ -221,3 +230,55 @@ def save_adata(adata, attr, key, data):
     """
     obj = getattr(adata, attr)
     obj[key] = data
+
+
+def prob_con(adata, overwrite=False, inplace=True):
+    if "prob_concatenated" in adata.obsm.keys():
+        warnings.warn("obsm['prob_concatenated'] already exists!")
+        if not overwrite:
+            return adata
+        else:
+            warnings.warn("overwriting obsm['prob_concatenated'].")
+            del adata.obsm["prob_concatenated"]
+    # Get a list of obsm matrices with names starting with "prob"
+    prob_matrices = [matrix_name for matrix_name in adata.obsm.keys() if matrix_name.startswith("prob")]
+    # Define a function to concatenate two matrices
+    def concatenate_matrices(matrix1, matrix2):
+        return np.concatenate((matrix1, matrix2), axis=1)
+    # Use functools.reduce to concatenate all matrices in prob_matrices
+    if prob_matrices:
+        concatenated_matrix = reduce(concatenate_matrices, [adata.obsm[matrix] for matrix in prob_matrices])
+        adata.obsm["prob_concatenated"] = concatenated_matrix
+        if inplace:
+            save_adata(adata_q, attr='obsm', key='spatial', data=concatenated_matrix)
+            return None
+    else:
+        warnings.warn("No 'prob' matrices found in the AnnData object.")
+    if not inplace:
+        return adata_q
+
+
+def map2ref(adata_ref, adata_q, k=10, key='spatial_pred', inplace=True):
+    if "spatial" not in adata_ref.obsm.keys():
+        raise Exception("adata_ref.obsm['spatial'] does not exist. Necessary for spatial mapping.")
+    if "prob_concatenated" not in adata_ref.obsm.keys():
+        warnings.warn("obsm['prob_concatenated'] does not exsit for adata_ref. Calculating obsm['prob_concatenated'].")
+        prob_con(adata_ref)
+    if "prob_concatenated" not in adata_q.obsm.keys():
+        warnings.warn("obsm['prob_concatenated'] does not exsit for adata_q. Calculating obsm['prob_concatenated'].")
+        prob_con(adata_q)
+    ref_embeddings = adata_ref.obsm['prob_concatenated']
+    kdtree_r1 = cKDTree(ref_embeddings)
+    target_embeddings = adata_q.obsm['prob_concatenated']
+    distances, indices = kdtree_r1.query(target_embeddings, k=k)
+    m             = Softmax(dim=1)
+    probs         = m(-torch.tensor(distances))
+    dist          = torch.distributions.categorical.Categorical(probs=probs)
+    idx           = dist.sample().numpy()
+    indices       = indices[np.arange(len(indices)), idx]
+    adata_q.obsm['spatial'] = adata_ref.obsm['spatial'][indices] 
+    if inplace:
+        save_adata(adata_q, attr='obsm', key='spatial', data= adata_ref.obsm['spatial'][indices])
+    else:
+        return adata_q
+    # swap.obs['sink']
